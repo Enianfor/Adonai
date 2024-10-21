e5a6382a3490beaff2807a8ea7633cd25266cc9a
luis13211 <165101448+luis13211@users.noreply.github.com>
2024-10-20 23:45:21 -0500

    Test

--- js/conexion.js
+++ js/conexion.js
@@ -1,25 +1,54 @@
-function APIunirseAlJuego() {
-    fetch("http://192.168.0.13:8080/unirse")
-        .then((res) => {
-            if(res.ok) {
-                res.text()
-                    .then((respuesta) => {
-                        console.log(respuesta)
-                        jugadorId = respuesta
-                    })
-            }
-        })
-}
-
-
-function APIseleccionarMokeponBackend(mascotaDelJugador) {
-    fetch(`http://192.168.0.13:8080/mokepon/${jugadorId}`, {
+function APIvalidarCodigoBarras(codigoBarras) {
+    fetch("http://192.168.0.13:8080/validar-codigo-barras", {
         method: "post",
         headers: {
             "content-Type": "application/json"
         },
         body: JSON.stringify({
-            mokepon: mascotaDelJugador
+            codigoBarras: codigoBarras
         })
     })
+    .then((res) => {
+        // Verificar si la respuesta es exitosa (código 200-299)
+        if (res.ok) {
+            // Accedemos al texto que nos envia el servidor
+            res.text()
+                .then((data) => {
+                    // Maneja la respuesta aquí
+                    console.log("Respuesta del servidor:", data);
+                })
+        }
+    })
+        
 }
+
+
+function APIenviarNumero(usuario, contraseña, codigoBarras) {
+    console.log("Se acaba de enviar datos")
+    fetch(`http://192.168.0.13:8080/registrar-producto`, {
+        method: "post",
+        headers: {
+            "content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            usuario: usuario,
+            contraseña: contraseña,
+            codigoBarras: codigoBarras
+        })
+    })
+    .then((res) => {
+        // Verificar si la respuesta es exitosa (código 200-299)
+        if (res.ok) {
+            // Accedemos al texto que nos envia el servidor
+            res.text()
+                .then((data) => {
+                    // Maneja la respuesta aquí
+                    console.log("Respuesta del servidor:", data);
+                })
+        }
+    })
+
+}
+
+APIenviarNumero("drako", "1234", 4444);
+APIvalidarCodigoBarras(789798);
